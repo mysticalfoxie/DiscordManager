91aca83d2630a5e0b23a6f22cad4e693ee5532b7
Michelle <MichelleBecker2003@gmail.com>
2021-07-04 14:57:57 +0200

    Add Command Event

---
+++ Events/CommandReceivedEvent.cs
@@ -0,0 +1,17 @@
+﻿using DCM.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCM.Events
+{
+    public class CommandReceivedEvent : IEvent
+    {
+        public CommandReceivedEvent()
+        {
+
+        }
+    }
+}
